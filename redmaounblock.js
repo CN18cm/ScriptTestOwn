d4a7c7f1057eb9c49ce1dc135e838f3439c2fe3e
kouichi <kouichikun.cn@gmail.com>
2022-11-24 21:55:54 +0800

    test1

--- redmaounblock.js
+++ redmaounblock.js
@@ -1,8 +1,6 @@
 //破解小红帽测试
 let obj = JSON.parse($response.body);
-obj.data = {
-    "is_lounge_vip":true,
-    "guest":true,
-    "im_role":1
-}
+obj.data.is_lounge_vip = true;
+obj.data.guest = true;
+obj.data.im_role = 1;
 $done({body:JSON.stringify(obj)});
